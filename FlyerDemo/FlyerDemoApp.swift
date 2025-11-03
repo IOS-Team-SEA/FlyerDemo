119b90b66b98830464bc964ce058c74756a76915
hkBeast <himanshu.singh@simplyentertaining.us>
2025-11-03 12:41:21 +0530

    Initial Commit

---
+++ FlyerDemo/FlyerDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlyerDemoApp.swift
+//  FlyerDemo
+//
+//  Created by HKBeast on 03/11/25.
+//
+
+import SwiftUI
+
+@main
+struct FlyerDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
